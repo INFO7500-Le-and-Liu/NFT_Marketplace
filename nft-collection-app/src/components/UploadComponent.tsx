83d07e16895170e0b2617e5290ab52125aad83ed
Le_Hong <144978918+leonle30@users.noreply.github.com>
2024-07-29 15:22:16 -0400

    test net
    mainly finished by ldy

--- nft-collection-app/src/components/UploadComponent.tsx
+++ nft-collection-app/src/components/UploadComponent.tsx
@@ -1,31 +1,36 @@
 import React, { useState } from 'react';
-
-// 替换为你的 Pinata 配置信息
-const JWT = process.env.REACT_APP_PINATA_JWT || ""; // 确保使用正确的 JWT
-
+import {mintNFT} from './MintNFT';
+ 
+const JWT = process.env.REACT_APP_PINATA_JWT || "";
+ 
 const UploadComponent: React.FC = () => {
   const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
-
+  // set price config
+  const [price, setPrice] = useState<string>("");  
+ 
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       setSelectedFiles(event.target.files);
     }
   };
-
+ 
+  const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPrice(event.target.value);  
+  };
+ 
   const pinDirectoryToPinata = async () => {
     if (!selectedFiles) {
       console.error("No files selected");
       return;
     }
-
+ 
     const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
     try {
       const data = new FormData();
-
       Array.from(selectedFiles).forEach((file) => {
         data.append(`file`, file, file.name);
       });
-
+ 
       const response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -33,26 +38,35 @@ const UploadComponent: React.FC = () => {
         },
         body: data
       });
-
+ 
       if (!response.ok) {
         console.error('Error response from Pinata:', await response.text());
         throw new Error('Failed to upload file to IPFS');
       }
-
+ 
       const resData = await response.json();
-      console.log(resData);
+      console.log('CID:', resData.IpfsHash);  // get and print CID
+ 
+      // mint NFT
+      try {
+          await mintNFT(resData.IpfsHash, price);
+          console.log("NFT has been minted successfully!");
+      } catch (mintError) {
+          console.error("Error minting NFT:", mintError);
+      }
     } catch (error) {
       console.error("Error uploading files:", error);
     }
   };
-
+ 
   return (
     <div>
       <label className="form-label">Choose Files</label>
       <input type="file" multiple onChange={changeHandler} />
-      <button onClick={pinDirectoryToPinata}>Upload to IPFS</button>
+      <input type="text" value={price} onChange={handlePriceChange} placeholder="Enter price" />
+      <button onClick={pinDirectoryToPinata}>Upload to IPFS and Mint NFT</button>
     </div>
   );
 };
-
+ 
 export default UploadComponent;
